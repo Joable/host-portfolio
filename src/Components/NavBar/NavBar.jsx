0b5da96405a62eb3fdaa89c2f8795ba438a36951
Joaquin <jaltable@fi.uba.ar>
2022-11-07 19:20:42 -0300

    Remade the 'Navbar' component with  react-bootstrap

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,29 @@
+import {Navbar as BsNavbar, Button}  from 'react-bootstrap'
+import { BsLinkedin } from "react-icons/bs"
+
+
 import './NavBar.css'
 
 function NavBar() {
-    return ( 
-        <div className="navbar-container">
-            <h1>Joaquin Altable</h1>
+    return (
+        <div className='nav-container'>
+            <BsNavbar bg="dark" variant='dark'>
+                <div className='nav-elements'>
+                    <div className='nav-left'>
+                        <BsNavbar.Brand href='/'>Joaquin Altable</BsNavbar.Brand>
+                        <BsNavbar.Text> 
+                        </BsNavbar.Text>
+                    </div>
+                    <div className="nav-right">
+                        <a href='https://www.linkedin.com/in/jealtable'>
+                            <BsLinkedin size={30}/>
+                        </a>
+                        <Button>Login</Button>
+                    </div>
+                </div>
+            </BsNavbar>
         </div>
+         
      );
 }
 
