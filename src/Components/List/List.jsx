2826c0053c6e74217f02c147dbef40b295a8811c
Joaquin <jaltable@fi.uba.ar>
2023-06-25 14:43:30 -0300

    created 'List' component

---
+++ src/Components/List/List.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+import { Card } from 'react-bootstrap';
+
+function List() {
+    return ( 
+        <Card>
+            <Card.Body>
+                <Card.Title>Title</Card.Title>
+                
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default List;
